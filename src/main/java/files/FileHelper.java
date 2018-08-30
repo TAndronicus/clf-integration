b94b043e4205baabe76016e6037d8cb6184aa1e1
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2018-08-30 22:40:29 +0200

    Add file reader

--- src/main/java/files/FileHelper.java
+++ src/main/java/files/FileHelper.java
@@ -2,17 +2,12 @@ package files;
 
 import data.Dataset;
 import de.bwaldvogel.liblinear.InvalidInputDataException;
-import de.bwaldvogel.liblinear.Problem;
 
-import java.io.File;
 import java.io.IOException;
 
 @FunctionalInterface
 public interface FileHelper {
 
-    default Dataset readFile(String filename) throws IOException, InvalidInputDataException {
-        Problem problem = Problem.readFromFile(new File(filename), 1);
-        return new Dataset(problem, problem, problem);
-    }
+    Dataset readFile(String filename) throws IOException, InvalidInputDataException;
 
 }
---
+++ src/main/java/files/FileReader10Way.java
@@ -0,0 +1,13 @@
+package files;
+
+import data.Dataset;
+import de.bwaldvogel.liblinear.InvalidInputDataException;
+
+import java.io.IOException;
+
+public class FileReader10Way implements FileHelper{
+    @Override
+    public Dataset readFile(String filename) throws IOException, InvalidInputDataException {
+        return null;
+    }
+}
