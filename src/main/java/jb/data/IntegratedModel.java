e7b9bb839ad46b5c2c2df684e01c1c1238331798
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2018-09-11 21:01:47 +0200

    Add integrated model class

---
+++ src/main/java/jb/data/IntegratedModel.java
@@ -0,0 +1,13 @@
+package jb.data;
+
+import lombok.Data;
+
+@Data
+public class IntegratedModel {
+
+    private double minX;
+    private double maxX;
+    private double[] a;
+    private double[] b;
+
+}
