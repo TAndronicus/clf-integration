7a859475491ae113a12379062ff259f44bf7695b
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2018-09-16 19:55:43 +0200

    Fix trainer for new dataset

--- src/main/java/jb/trainer/SvmTrainer.java
+++ src/main/java/jb/trainer/SvmTrainer.java
@@ -3,21 +3,20 @@ package jb.trainer;
 import de.bwaldvogel.liblinear.Linear;
 import de.bwaldvogel.liblinear.Model;
 import de.bwaldvogel.liblinear.Parameter;
-import de.bwaldvogel.liblinear.Problem;
 import jb.config.Opts;
 import jb.data.Dataset;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SVMTrainer implements Trainer{
+public class SvmTrainer implements Trainer{
 
     @Override
     public List<Model> train(Dataset dataset, Opts opts) {
         Parameter parameter = new Parameter(opts.getSolverType(), opts.getC(), opts.getEps());
         List<Model> clfs = new ArrayList<>();
-        for (Problem problem : dataset.getTrainingProblems()) {
-            clfs.add(Linear.train(problem, parameter));
+        for (jb.data.Problem problem : dataset.getProblems()) {
+            clfs.add(Linear.train(problem.convertToLibLinearProblem(), parameter));
         }
         return clfs;
     }
