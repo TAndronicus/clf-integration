689fd6253fabe2d27628e7870ec64c4e60990c68
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2018-09-17 22:53:36 +0200

    Fix tester

--- src/main/java/jb/tester/IntegratedScoreTester.java
+++ src/main/java/jb/tester/IntegratedScoreTester.java
@@ -1,24 +1,27 @@
 package jb.tester;
 
+import jb.config.Opts;
 import jb.data.Dataset;
 import jb.data.IntegratedModel;
+import jb.data.Problem;
+import jb.data.ValidatingTestingTuple;
 
 import static jb.util.ModelUtils.getIndexOfSubspace;
 
 public class IntegratedScoreTester {
 
-    public double test(IntegratedModel integratedModel, Dataset dataset) {
-        /*double counter = 0;
-        for (int i = 0; i < dataset.getTestingProblem().l; i++) {
-            int index = getIndexOfSubspace(integratedModel.getA().length, dataset.getTestingProblem().x[i][0].getValue(), dataset.getMinX(), dataset.getMaxX());
-            double value = integratedModel.getA()[index] * dataset.getTestingProblem().x[i][0].getValue() + integratedModel.getB()[index];
-            if (value > dataset.getTestingProblem().x[i][1].getValue() ^ dataset.getTestingProblem().y[i] == 1) {
+    public double test(IntegratedModel integratedModel, ValidatingTestingTuple validatingTestingTuple, Opts opts) {
+        double counter = 0;
+        Problem problem = validatingTestingTuple.getTestingProblem();
+        for (int i = 0; i < problem.getY().length; i++) {
+            int index = getIndexOfSubspace(opts.getNumberOfSpaceParts(), problem.getX()[i][0], validatingTestingTuple.getMinX(), validatingTestingTuple.getMaxX());
+            double value = integratedModel.getA()[index] * problem.getX()[i][0] + integratedModel.getB()[index];
+            if (value > problem.getX()[i][1] ^ problem.getY()[i] == 1) {
                 counter++;
             }
         }
-        double score = counter / dataset.getTestingProblem().l;
+        double score = counter / problem.getY().length;
         score = score > .5 ? score : 1 - score;
-        return score;*/
-        return 0;
+        return score;
     }
 }
