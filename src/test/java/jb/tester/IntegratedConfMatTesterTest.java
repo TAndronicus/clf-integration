f948982a95d02b8d4a0c1416244c583fecdef335
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2018-09-18 22:15:45 +0200

    Fix tests

--- src/test/java/jb/tester/IntegratedConfMatTesterTest.java
+++ src/test/java/jb/tester/IntegratedConfMatTesterTest.java
@@ -1,17 +1,17 @@
 package jb.tester;
 
-import de.bwaldvogel.liblinear.Feature;
-import de.bwaldvogel.liblinear.FeatureNode;
-import de.bwaldvogel.liblinear.Problem;
+import jb.config.Opts;
 import jb.data.Dataset;
 import jb.data.IntegratedModel;
-import jb.util.ModelUtils;
+import jb.data.Problem;
 import org.junit.jupiter.api.Test;
-import static org.hamcrest.Matchers.*;
 
 import java.util.ArrayList;
 
+import static jb.util.ModelUtils.calculateScoreFromConfMat;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 public class IntegratedConfMatTesterTest {
 
@@ -19,35 +19,35 @@ public class IntegratedConfMatTesterTest {
     public void testShouldCalculateRightConfMat() {
 
         // given
+        Opts opts = Opts.builder().permutation(new int[]{0, 0}).numberOfSpaceParts(3).build();
         double[] a = {1, .5, 2};
         double[] b = {.5, 0, -.5};
         double minX = -1;
         double maxX = 2;
         IntegratedModel integratedModel = IntegratedModel.builder().a(a).b(b).build();
-        Feature[] feature0 = {new FeatureNode(0, .2), new FeatureNode(1, 0)};
-        Feature[] feature1 = {new FeatureNode(0, .5), new FeatureNode(1, .3)};
-        Feature[] feature2 = {new FeatureNode(0, .8), new FeatureNode(1, .3)};
-        Feature[] feature3 = {new FeatureNode(0, -.5), new FeatureNode(1, .1)};
-        Feature[] feature4 = {new FeatureNode(0, 1.5), new FeatureNode(1, 2)};
-        double[] y = {0, 0, 1, 1, 0}; // should be {0, 1, 0, 1, 0}, expected score 1 - 0.4 = 0.6
-        Problem problem = new Problem();
-        problem.x = new Feature[][]{feature0, feature1, feature2, feature3, feature4};
-        problem.y = y;
-        problem.l = problem.x.length;
-        //Dataset dataset = new Dataset(new ArrayList<>(), new ArrayList<>(), problem, minX, maxX);
+        double[][] x = {
+                {.2, 0},
+                {.5, .3},
+                {.8, .3},
+                {-.5, .1},
+                {1.5, 2}
+        };
+        int[] y = {0, 0, 1, 1, 0}; // should be {0, 1, 0, 1, 0}, expected score 1 - 0.4 = 0.6
+        Problem problem = new Problem(x, y);
+        Dataset dataset = new Dataset(new ArrayList<Problem>(){{add(problem);}}, minX, maxX);
         IntegratedScoreTester integratedScoreTester = new IntegratedScoreTester();
-        //double score = integratedScoreTester.test(integratedModel, dataset);
+        double score = integratedScoreTester.test(integratedModel, dataset.getValidatingTestingTuple(opts), opts);
 
         // when
         IntegratedConfMatTester integratedConfMatTester = new IntegratedConfMatTester();
-        //int[][] confMat = integratedConfMatTester.test(integratedModel, dataset);
-        //double scoreFromConfMat = ModelUtils.calculateScoreFromConfMat(confMat);
+        int[][] confMat = integratedConfMatTester.test(integratedModel, dataset.getValidatingTestingTuple(opts), opts);
+        double scoreFromConfMat = calculateScoreFromConfMat(confMat);
 
         // then
-        /*assertThat(scoreFromConfMat, is(equalTo(score)));
+        assertThat(scoreFromConfMat, is(equalTo(score)));
         assertThat(confMat[0][0], is(equalTo(2)));
         assertThat(confMat[0][1], is(equalTo(1)));
         assertThat(confMat[1][0], is(equalTo(1)));
-        assertThat(confMat[1][1], is(equalTo(1)));*/
+        assertThat(confMat[1][1], is(equalTo(1)));
     }
 }
--- src/test/java/jb/tester/IntegratedScoreTesterTest.java
+++ src/test/java/jb/tester/IntegratedScoreTesterTest.java
@@ -1,45 +1,45 @@
 package jb.tester;
 
-import de.bwaldvogel.liblinear.Feature;
-import de.bwaldvogel.liblinear.FeatureNode;
-import de.bwaldvogel.liblinear.Problem;
+import jb.config.Opts;
 import jb.data.Dataset;
 import jb.data.IntegratedModel;
+import jb.data.Problem;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 public class IntegratedScoreTesterTest {
 
     @Test
     public void shouldReturnRightScore() {
         // given
+        Opts opts = Opts.builder().permutation(new int[]{0, 0}).numberOfSpaceParts(3).build();
         double[] a = {1, .5, 2};
         double[] b = {.5, 0, -.5};
         double minX = -1;
         double maxX = 2;
         IntegratedModel integratedModel = IntegratedModel.builder().a(a).b(b).build();
-        Feature[] feature0 = {new FeatureNode(0, .2), new FeatureNode(1, 0)};
-        Feature[] feature1 = {new FeatureNode(0, .5), new FeatureNode(1, .3)};
-        Feature[] feature2 = {new FeatureNode(0, .8), new FeatureNode(1, .3)};
-        Feature[] feature3 = {new FeatureNode(0, -.5), new FeatureNode(1, .1)};
-        Feature[] feature4 = {new FeatureNode(0, 1.5), new FeatureNode(1, 2)};
-        double[] y = {0, 0, 1, 1, 0}; // should be {0, 1, 0, 1, 0}, expected score 1 - 0.4 = 0.6
-        Problem problem = new Problem();
-        problem.x = new Feature[][]{feature0, feature1, feature2, feature3, feature4};
-        problem.y = y;
-        problem.l = problem.x.length;
-        //Dataset dataset = new Dataset(new ArrayList<>(), new ArrayList<>(), problem, minX, maxX);
+        double[][] x = {
+                {.2, 0},
+                {.5, .3},
+                {.8, .3},
+                {-.5, .1},
+                {1.5, 2}
+        };
+        int[] y = {0, 0, 1, 1, 0}; // should be {0, 1, 0, 1, 0}, expected score 1 - 0.4 = 0.6
+        Problem problem = new Problem(x, y);
+        Dataset dataset = new Dataset(new ArrayList<Problem>(){{add(problem);}}, minX, maxX);
 
         // when
         IntegratedScoreTester integratedScoreTester = new IntegratedScoreTester();
-        //double score = integratedScoreTester.test(integratedModel, dataset);
+        double score = integratedScoreTester.test(integratedModel, dataset.getValidatingTestingTuple(opts), opts);
 
         // then
-        //assertThat(score, is(equalTo(0.6)));
+        assertThat(score, is(equalTo(0.6)));
     }
 
 }
