282a683ff01dc6c98b2d0e4e085ab4b70f8769a7
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2018-09-18 21:36:56 +0200

    Optimize feature conversion

--- src/main/java/jb/data/Problem.java
+++ src/main/java/jb/data/Problem.java
@@ -16,14 +16,14 @@ public class Problem {
     public de.bwaldvogel.liblinear.Problem convertToLibLinearProblem(Opts opts) {
         de.bwaldvogel.liblinear.Problem problem = new de.bwaldvogel.liblinear.Problem();
         problem.bias = opts.getBias();
-        problem.l = x.length;
-        problem.n = x[0].length;
-        Feature[][] features = new Feature[x.length][x[0].length];
+        problem.l = this.x.length;
+        problem.n = this.x[0].length;
+        Feature[][] features = new Feature[this.x.length][this.x[0].length];
         double[] classes = new double[problem.l];
         for (int i = 0; i < problem.l; i++) {
             Feature[] baseFeature = new Feature[problem.n];
             for (int j = 0; j < problem.n; j++) {
-                baseFeature[j] = new FeatureNode(j + 1, x[i][j]);
+                baseFeature[j] = new FeatureNode(j + 1, this.x[i][j]);
             }
             features[i] = baseFeature;
             classes[i] = this.y[i];
@@ -31,6 +31,14 @@ public class Problem {
         problem.x = features;
         problem.y = classes;
         return problem;
+    }
+
+    public Feature[] getObjectAsFeatures(int index) {
+        Feature[] features = new Feature[this.x[index].length];
+        for (int i = 0; i < this.x[index].length; i++) {
+            features[i] = new FeatureNode(i + 1, this.x[index][i]);
+        }
+        return features;
     }
 
 }
