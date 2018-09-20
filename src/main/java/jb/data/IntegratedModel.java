b0a0ba3f0f85efdfb6f929ff6a8b7d08314069a1
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2018-09-20 20:16:11 +0200

    Add space partition to integrated model

--- src/main/java/jb/data/IntegratedModel.java
+++ src/main/java/jb/data/IntegratedModel.java
@@ -11,5 +11,16 @@ public class IntegratedModel {
     private double maxX;
     private double[] a;
     private double[] b;
+    /**
+     * Points between which the discriminant limit has the same function
+     * Span                | a        | b
+     * minX - x[0]         | a[0]     | b[0]
+     * x[0] - x[1]         | a[1]     | b[1]
+     * x[1] - x[2]         | a[2]     | b[2]
+     *     ...             | ...      | ...
+     * x[n - 2] - x[n - 1] | a[n - 1] | b[n - 1]
+     * x[n - 1] - maxX     | a[n]     | n[n]
+     */
+    private double[] x;
 
 }
