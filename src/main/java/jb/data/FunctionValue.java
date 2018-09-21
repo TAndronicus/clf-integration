c75432df0ae602f4633ba61bc13f846fa44eb668
jbiedrzycki <jedrzej.biedrzycki@iteratec.pl>
2018-09-21 20:56:51 +0200

    Add function value for median integrator

---
+++ src/main/java/jb/data/FunctionValue.java
@@ -0,0 +1,13 @@
+package jb.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FunctionValue {
+
+    private int index;
+    private double value;
+
+}
