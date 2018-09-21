3fe495097490cf1152f4e0d09cc195b4c5f2250e
jbiedrzycki <jedrzej.biedrzycki@iteratec.pl>
2018-09-21 20:57:34 +0200

    Median integrator

--- src/main/java/jb/integrator/MedianIntegrator.java
+++ src/main/java/jb/integrator/MedianIntegrator.java
@@ -3,14 +3,36 @@ package jb.integrator;
 import de.bwaldvogel.liblinear.Model;
 import jb.config.Opts;
 import jb.data.Dataset;
+import jb.data.FunctionValue;
 import jb.data.IntegratedModel;
 import jb.data.SelectedTuple;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static jb.util.ModelUtils.*;
 
 public class MedianIntegrator implements Integrator{
     @Override
     public IntegratedModel integrate(SelectedTuple selectedTuple, List<Model> clfs, Dataset dataset, Opts opts) {
+
+        double[] as = getAs(clfs);
+        double[] bs = getBs(clfs);
+        List<Double> x = new ArrayList<>();
+        for (int i = 0; i < opts.getNumberOfSpaceParts(); i++) {
+            List<Double> crosspoints = getAllCrosspoints(as, bs);
+            List<Double> filteredCrosspoints = filterCrosspoints(crosspoints, dataset, i, opts);
+            // crosspoints sorting
+            for (int j = 0; j < filteredCrosspoints.size() - 1; j++) {
+                List<FunctionValue> functionValues = new ArrayList<>();
+                for (int k = 0; k < as.length; k++) {
+                    functionValues.add(new FunctionValue(k, as[k] * (crosspoints.get(j) + crosspoints.get(j + 1)) / 2 + bs[k]));
+                }
+            }
+        }
         return null;
     }
+
 }
