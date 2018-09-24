a13b3bb1f72ec6a922a4028f7108a2b7fbc1246e
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2018-09-24 18:47:03 +0200

    Add tests

---
+++ src/test/java/jb/util/MathUtilsTest.java
@@ -0,0 +1,111 @@
+package jb.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+
+    @Test
+    public void testShouldReturnVectorProduct() {
+
+        // given
+        double[] v0 = {0, 1, 2, 3};
+        double[] v1 = {4, 3, 2, 1};
+
+        // when
+        double product = MathUtils.vectorProduct(v0, v1);
+
+        // then
+        assertThat(product, is(equalTo(10.0)));
+    }
+
+    @Test
+    public void testShouldReturnTrace() {
+
+        // given
+        double[] vector = {.5, .7, .9, 1.1};
+
+        // when
+        double trace = MathUtils.vectorTrace(vector);
+
+        // then
+        assertThat(trace, is(equalTo(3.2)));
+    }
+
+    @Test
+    public void testShouldReturnRightCombinations() {
+
+        // given
+        int range = (int) (100 * Math.random());
+
+        // when
+        List<int[]> combinations = MathUtils.getCombinationsOfTwo(range);
+
+        // then
+        assertThat(combinations.size(), is(equalTo(range * (range - 1))));
+    }
+
+    @Test
+    public void testNoPermutationsOneEl() {
+
+        // given
+        int[] v0 = {2};
+        int[] v1 = {3};
+
+        // when
+        boolean arePermutations = MathUtils.arePermutations(v0, v1);
+
+        // then
+        assertThat(arePermutations, is(not(true)));
+    }
+
+    @Test
+    public void testPermutationsOneEl() {
+
+        // given
+        int[] v0 = {2};
+        int[] v1 = {2};
+
+        // when
+        boolean arePermutations = MathUtils.arePermutations(v0, v1);
+
+        // then
+        assertThat(arePermutations, is(true));
+    }
+
+    @Test
+    public void testNoPermutationsTwoEl() {
+
+        // given
+        int[] v0 = {2, 1};
+        int[] v1 = {3, 2};
+
+        // when
+        boolean arePermutations = MathUtils.arePermutations(v0, v1);
+
+        // then
+        assertThat(arePermutations, is(not(true)));
+    }
+
+    @Test
+    public void testPermutationsTwoEl() {
+
+        // given
+        int[] v0 = {2, 3};
+        int[] v1 = {3, 2};
+
+        // when
+        boolean arePermutations = MathUtils.arePermutations(v0, v1);
+
+        // then
+        assertThat(arePermutations, is(true));
+    }
+
+}
--- src/test/java/jb/util/ModelUtilsTest.java
+++ src/test/java/jb/util/ModelUtilsTest.java
@@ -4,9 +4,53 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.in;
 import static org.hamcrest.Matchers.is;
 
 public class ModelUtilsTest {
+
+    @Test
+    public void testShouldReturnRightSubspaceIndex() {
+
+        // given
+        double minX = 0;
+        double maxX = 10;
+        int numberOfSubspaces = 5;
+        double[] testX = {0, 1, 5, 7, 10};
+        int[] expectedIndices = {0, 0, 2, 3, 4};
+
+        // when
+        int[] indices = new int[expectedIndices.length];
+        for (int i = 0; i < indices.length; i++) {
+            indices[i] = ModelUtils.getIndexOfSubspace(numberOfSubspaces, testX[i], minX, maxX);
+        }
+
+        // then
+        for (int i = 0; i < indices.length; i++) {
+            assertThat(indices[i], is(equalTo(expectedIndices[i])));
+        }
+    }
+
+    @Test
+    public void testShouldReturnRightSubspaceIndexVector() {
+
+        // given
+        double[] x = {1, 2, 3, 5, 8, 13};
+        double[] samples = {1, 2.5, 9, 13};
+        int[] expectedIndices = {0, 1, 4, 4};
+
+        // when
+        int[] indices = new int[samples.length];
+        for (int i = 0; i < expectedIndices.length; i++) {
+            indices[i] = ModelUtils.getIndexOfSubspace(x, samples[i]);
+        }
+
+        // then
+        for (int i = 0; i < indices.length; i++) {
+            assertThat(indices[i], is(equalTo(expectedIndices[i])));
+        }
+
+    }
 
     @Test
     public void testShouldCalculateRightScore() {
@@ -20,5 +64,7 @@ public class ModelUtilsTest {
         // then
         assertThat(score, is(equalTo((.0 + confMat[0][0] + confMat[1][1]) / (confMat[0][0] + confMat[0][1] + confMat[1][0] + confMat[1][1]))));
     }
+
+
 
 }
