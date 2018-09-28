5dbf6a0398354a7494a0a40765d0607a30815746
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2018-09-28 19:33:41 +0200

    Move to kotlin

---
+++ src/main/java/jb/config/Opts.kt
@@ -0,0 +1,23 @@
+package jb.config
+
+import de.bwaldvogel.liblinear.SolverType
+
+data class Opts(val filePath: String?, //Absolute path to file in LibSVM format
+                val bias: Double, //Non-positive bias means fixing classifier discriminator function in (0, 0, ..., 0)
+                val numberOfBaseClassifiers: Int, //Number of base classifiers
+                val numberOfSelectedClassifiers: Int, // Number of selected classifiers
+                val numberOfSpaceParts: Int, // Number of space parts
+                val solverType: SolverType?, // Solver type
+                val c: Double, // Cost of constraints violation
+                val eps: Double, // Stopping criteria
+                val permutation: IntArray? // [i, j], where i - index of validation dataset, j - index of testing dataset
+) {
+
+    val filename: String
+        get() {
+            val pathParts = this.filePath!!.split("/".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            val nameParts = pathParts[pathParts.size - 1].split("[.]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            return nameParts[0]
+        }
+
+}
---
+++ src/main/java/jb/runner/Runner.kt
@@ -0,0 +1,66 @@
+package jb.runner
+
+import de.bwaldvogel.liblinear.InvalidInputDataException
+import de.bwaldvogel.liblinear.Model
+import de.bwaldvogel.liblinear.SolverType
+import jb.config.Opts
+import jb.data.*
+import jb.files.BReader
+import jb.files.FileHelper
+import jb.integrator.Integrator
+import jb.integrator.MeanIntegrator
+import jb.selector.NBestSelector
+import jb.selector.Selector
+import jb.tester.IntegratedScoreTester
+import jb.trainer.SvmTrainer
+import jb.trainer.Trainer
+import jb.validator.SimpleScoreValidator
+import jb.validator.Validator
+
+import java.io.IOException
+import java.util.*
+
+import jb.util.BUtils.getAllPermutations
+import jb.util.MathUtils.getCombinationsOfTwo
+
+object Runner {
+
+    private var fileHelper: FileHelper = BReader()
+    private var trainer: Trainer = SvmTrainer()
+    private var validator: Validator = SimpleScoreValidator()
+    private var selector: Selector = NBestSelector()
+    private var integrator: Integrator = MeanIntegrator()
+
+    @Throws(IOException::class, InvalidInputDataException::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+
+        val combinations = getCombinationsOfTwo(11)
+        val finalScores = TreeSet<Double>()
+        for (combination in combinations) {
+            val permutations = getAllPermutations()
+            val scores = TreeMap<Double, IntArray>()
+            for (permutation in permutations) {
+                val opts = Opts.builder().filePath("src/main/resources/target/7_" + permutation[0] + "_" + permutation[1] + "_converted.csv").bias(1.0).numberOfBaseClassifiers(9).numberOfSelectedClassifiers(3).numberOfSpaceParts(3).solverType(SolverType.L2R_LR).C(1.0).eps(.01).permutation(combination).build()
+                val dataset = fileHelper.readFile(opts)
+                val clfs = trainer.train(dataset, opts)
+                val validatingTestingTuple = dataset.getValidatingTestingTuple(opts)
+                val scoreTuple = validator.validate(clfs, validatingTestingTuple, opts)
+                val selectedTuple = selector.select(scoreTuple, opts)
+                val integratedModel = integrator.integrate(selectedTuple, clfs, dataset, opts)
+                val integratedScoreTester = IntegratedScoreTester()
+                scores[integratedScoreTester.test(integratedModel, validatingTestingTuple, opts)] = permutation
+            }
+            finalScores.add(scores.lastKey())
+        }
+        var sum = 0.0
+        for (finalScore in finalScores) {
+            sum += finalScore
+        }
+        println(sum / finalScores.size)
+        println(finalScores.last())
+
+    }
+
+}
