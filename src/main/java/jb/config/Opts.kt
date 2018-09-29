97812f261cf30b5742792fc74bd33a52f963f6f4
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2018-09-29 19:28:38 +0200

    Remove lombok

--- src/main/java/jb/config/Opts.kt
+++ src/main/java/jb/config/Opts.kt
@@ -2,15 +2,15 @@ package jb.config
 
 import de.bwaldvogel.liblinear.SolverType
 
-data class Opts(val filePath: String?, //Absolute path to file in LibSVM format
-                val bias: Double, //Non-positive bias means fixing classifier discriminator function in (0, 0, ..., 0)
+data class Opts(val filePath: String, //Absolute path to file in LibSVM format
+                val bias: Double = 1.0, //Non-positive bias means fixing classifier discriminator function in (0, 0, ..., 0)
                 val numberOfBaseClassifiers: Int, //Number of base classifiers
                 val numberOfSelectedClassifiers: Int, // Number of selected classifiers
                 val numberOfSpaceParts: Int, // Number of space parts
-                val solverType: SolverType?, // Solver type
-                val c: Double, // Cost of constraints violation
-                val eps: Double, // Stopping criteria
-                val permutation: IntArray? // [i, j], where i - index of validation dataset, j - index of testing dataset
+                val solverType: SolverType = SolverType.L2R_LR, // Solver type
+                val c: Double = 1.0, // Cost of constraints violation
+                val eps: Double = .01, // Stopping criteria
+                val permutation: IntArray // [i, j], where i - index of validation dataset, j - index of testing dataset
 ) {
 
     fun getFilename(): String {
